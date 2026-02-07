cb3bfb2f929cd1518c437ff01671dbc1e2011a16
Lucas D. <lucasdomingosleao55@hotmail.com>
2026-02-07 12:19:23 -0300

    Testes iniciais Model Orders

--- RESTAPI_TMB2026/Program.cs
+++ RESTAPI_TMB2026/Program.cs
@@ -1,8 +1,11 @@
+using RESTAPI_TMB2026;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -16,10 +19,12 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+
+app.OrdersRoutes();
+
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
 app.MapControllers();
-
 app.Run();
