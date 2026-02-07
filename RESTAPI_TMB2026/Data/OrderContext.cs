ec9ebe329c54bb511562bf01f36b27a4c58a758f
Lucas D. <lucasdomingosleao55@hotmail.com>
2026-02-07 13:19:47 -0300

    Add Sqlite EF + Inicio Migrations

--- RESTAPI_TMB2026/Data/OrderContext.cs
+++ RESTAPI_TMB2026/Data/OrderContext.cs
@@ -1,7 +1,17 @@
-﻿namespace RESTAPI_TMB2026.Data
-{
-    public class OrderContext
-    {
+﻿using Microsoft.EntityFrameworkCore;
+using RESTAPI_TMB2026.Models;
 
+namespace RESTAPI_TMB2026.Data
+{
+    public class OrderContext : DbContext
+
+    {
+        public DbSet<OrderModel> Orders { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=orders.db");
+            base.OnConfiguring(optionsBuilder);
+        }
     }
 }
--- RESTAPI_TMB2026/Program.cs
+++ RESTAPI_TMB2026/Program.cs
@@ -1,10 +1,12 @@
 using RESTAPI_TMB2026;
+using RESTAPI_TMB2026.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<OrderContext>();
 
 var app = builder.Build();
 
@@ -14,7 +16,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+ 
 
 app.OrdersRoutes();
 app.UseHttpsRedirection();
