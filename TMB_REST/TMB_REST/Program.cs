5f5faf870c44e8cc98b9986c7e93c39d109a526a
Lucas D. <lucasdomingosleao55@hotmail.com>
2026-02-08 19:42:21 -0300

    add fix CORS

--- TMB_REST/TMB_REST/Program.cs
+++ TMB_REST/TMB_REST/Program.cs
@@ -1,7 +1,20 @@
+using System.Web.Http;
 using TMB_REST;
 using TMB_REST.Data;
 
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      policy =>
+                      {
+                          policy.WithOrigins("http://localhost:3000",
+                                              "https://localhost:44392");
+                      });
+});
 
 // Add services to the container.
 
@@ -17,13 +30,19 @@ if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
+
+
 app.MapGet("/test", () => "Hello World!");
 
 app.OrdersRoutes();
 app.UseHttpsRedirection();
 
+
+app.UseCors(MyAllowSpecificOrigins);
+
 app.UseAuthorization();
 
 app.MapControllers();
-
 app.Run();
+
+
